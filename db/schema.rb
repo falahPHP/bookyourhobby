ef3d329c277736d8c756227422a72f1b65dfcdc6
falahaboud <falah.aboud@gmail.com>
2019-10-28 22:01:36 +0100

    Added full name with strong parameter

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_27_181246) do
+ActiveRecord::Schema.define(version: 2019_10_28_203033) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -20,6 +20,11 @@ ActiveRecord::Schema.define(version: 2019_10_27_181246) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "full_name"
+    t.string "from"
+    t.text "about"
+    t.string "language"
+    t.boolean "status"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
