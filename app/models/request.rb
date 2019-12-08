5d97f3680bcfa0ed4dd0761f2d5592ad1b1e4b93
falahaboud <falah.aboud@gmail.com>
2019-12-08 23:51:05 +0100

    Request programmiert fertig

---
+++ app/models/request.rb
@@ -0,0 +1,11 @@
+class Request < ApplicationRecord
+  belongs_to :user
+  belongs_to :category
+
+  has_one_attached :attachment_file
+
+  validates :title, presence: { message: "cannot be empty"}
+  validates :description, presence: { message: "cannot be empty"}
+  validates :delivery, numericality:  { only_integer:true,  message: "cannot be empty"}
+  
+end
